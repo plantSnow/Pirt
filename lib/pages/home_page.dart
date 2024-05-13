43de1cfaa925b42a55f6815a02b829c928b28e51
NingZhonghao <69165342@qq.com>
2024-05-13 20:04:41 +0800

    不知道电脑神经，懂不懂卡住键盘输入，我决定重启一次。

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,23 @@
+/*
+ * @Author: NingZhonghao 69165342@qq.com
+ * @Date: 2024-05-13 17:25:19
+ * @LastEditors: NingZhonghao 69165342@qq.com
+ * @LastEditTime: 2024-05-13 20:00:14
+ * @FilePath: \pirt\lib\pages\home_page.dart
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
